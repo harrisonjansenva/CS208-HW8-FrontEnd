2efbc6a92673925fbf11c9ae1bca5c7dc6332d36
Harrison Jansen van Beek <harrisonjansenva@u.boisestate.edu>
2024-12-05 17:10:09 -0700

    added initial functionality and constants for elements from DOM

--- students.js
+++ students.js
@@ -1,3 +1,12 @@
 console.log('students.js is executing...');
 
 // TODO: add your code here
+
+const id_form_create_new_student = document.getElementById('id_form_create_new_student');
+id_form_create_new_student.addEventListener('submit', handleCreateNewStudentEvent);
+
+const div_create_new_student = document.getElementById('create_new_student');
+const div_show_student_details = document.getElementById('show_student_details');
+const div_update_student_details = document.getElementById('update_student_details');
+const div_delete_student = document.getElementById('delete_student');
+const div_list_of_students = document.getElementById("list_of_students");
