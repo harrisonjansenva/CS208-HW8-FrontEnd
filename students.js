4cd59a48ff776d72f78a7ee7a2c2c60522a7d9c7
Harrison Jansen van Beek <harrisonjansenva@u.boisestate.edu>
2024-12-05 17:11:24 -0700

    added functionality for getAndDisplayAllStudents()

--- students.js
+++ students.js
@@ -10,3 +10,38 @@ const div_show_student_details = document.getElementById('show_student_details')
 const div_update_student_details = document.getElementById('update_student_details');
 const div_delete_student = document.getElementById('delete_student');
 const div_list_of_students = document.getElementById("list_of_students");
+document.addEventListener("DOMContentLoaded", async function () {
+    await getAndDisplayAllStudents();
+});
+
+
+
+async function getAndDisplayAllStudents() {
+    console.log(`getAndDisplayAllStudents - START`);
+    const API_URL = "http://localhost:8080/students";
+
+    div_list_of_students.innerHTML = "Calling the API to get the list of students...";
+
+    try {
+        const response = await fetch(API_URL);
+
+        console.log({response});
+        console.log(`response.status = ${response.status}`);
+        console.log(`response.statusText = ${response.statusText}`);
+        console.log(`response.ok = ${response.ok}`);
+        if (response.ok) {
+            div_list_of_students.innerHTML = "Retrieved Students, not just processing....";
+
+            const listOfStudentsAsJSON = await response.json();
+            console.log({listOfStudents: listOfStudentsAsJSON});
+
+            displayStudents(listOfStudentsAsJSON);
+        } else {
+            div_list_of_students.innerHTML = `<p class ="failure">ERROR: failed to retrieve the students.</p>`;
+        }
+    } catch (error) {
+        console.error(error);
+        div_list_of_classes.innerHTML = '<p class="failure">ERROR: failed to connect to the API to fetch the students data.</p>';
+    }
+    console.log(`getAndDisplayAllStudents - END`);
+}
