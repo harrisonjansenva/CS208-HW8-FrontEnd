11157f5475088240093c3d6cd732722d34fc85ec
Harrison Jansen van Beek <harrisonjansenva@u.boisestate.edu>
2024-12-05 17:11:50 -0700

    added functionality for createNewStudent()

--- students.js
+++ students.js
@@ -10,6 +10,7 @@ const div_show_student_details = document.getElementById('show_student_details')
 const div_update_student_details = document.getElementById('update_student_details');
 const div_delete_student = document.getElementById('delete_student');
 const div_list_of_students = document.getElementById("list_of_students");
+
 document.addEventListener("DOMContentLoaded", async function () {
     await getAndDisplayAllStudents();
 });
@@ -44,4 +45,31 @@ async function getAndDisplayAllStudents() {
         div_list_of_classes.innerHTML = '<p class="failure">ERROR: failed to connect to the API to fetch the students data.</p>';
     }
     console.log(`getAndDisplayAllStudents - END`);
+}
+async function createNewStudent(studentData) {
+    const API_URL = 'http://localhost:8080/students';
+    try {
+        const response = await fetch(API_URL, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded"
+            },
+            body: new URLSearchParams(studentData)
+        });
+        console.log({response});
+        console.log(`response.status = ${response.status}`);
+        console.log(`response.statusText = ${response.statusText}`);
+        console.log(`response.ok = ${response.ok}`);
+
+        if (response.ok) {
+            const createdStudent = await response.json();
+            div_create_new_student.innerHTML = `<p class="success">Student with id ${createdStudent.id} created successfully. </p>`;
+            await getAndDisplayAllStudents();
+        } else {
+            div_create_new_student.innerHTML = '<p class="failure">ERROR';
+        }
+    } catch (error) {
+        console.error(error);
+        div_create_new_student.innerHTML = `<p class="failure">ERROR: failed to connect to API to create new student.</p>`;
+    }
 }
