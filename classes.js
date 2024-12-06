c88a86d17fee5575df942afce152ff3f77cb78f0
Harrison Jansen van Beek <harrisonjansenva@u.boisestate.edu>
2024-12-05 17:17:54 -0700

    fixed a couple bugs in classes.js

--- classes.js
+++ classes.js
@@ -10,11 +10,10 @@ const div_delete_class = document.getElementById("delete_class");
 const div_list_of_classes = document.getElementById("list_of_classes");
 
 
-// // TODO: uncomment the following code to fetch and display the classes after the page loads
-// document.addEventListener("DOMContentLoaded", async function()
-// {
-//     await getAndDisplayAllClasses();
-// });
+document.addEventListener("DOMContentLoaded", async function()
+{
+    await getAndDisplayAllClasses();
+});
 
 
 // =====================================================================================================================
